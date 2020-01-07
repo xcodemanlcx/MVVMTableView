77977519505f4a8a5039041ea702bac99cfea366
lcx <leichunxiang58@163.com>
2020-01-07 17:40:54 +0800

    delete pics and LCXTableViewKit.h optimization

--- MVVMTableView/LCXTableViewKit/LCXTableViewKit.h
+++ MVVMTableView/LCXTableViewKit/LCXTableViewKit.h
@@ -9,12 +9,8 @@
 #ifndef LCXTableViewKit_h
 #define LCXTableViewKit_h
 
-#import "LCXInitTableView.h"
-// 注册cell（免命名复用cell的id）
 #import "UITableView+LCXRegisterCellClasses.h"
-// 处理cell的响应事件
 #import "UITableView+LCXHandleActionCell.h"
-// 滚动到顶部
 #import "UITableView+LCXScroll.h"
 
 #endif /* LCXTableViewKit_h */
